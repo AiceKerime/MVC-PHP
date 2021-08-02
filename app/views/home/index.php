3ec937a6bea98df3d62c0cd65edbfd33910cdbef
rlf990 <53922794+rlf990@users.noreply.github.com>
2021-08-02 14:19:40 +0700

    Update index.php

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,1 +1,11 @@
-<h1>Welcome in My Website</h1>
+<div class="container">
+
+    <div class="jumbotron mt-5">
+        <h1 class="display-4">Welcome in My Website</h1>
+        <p class="lead"></p>
+        <hr class="my-4">
+        <p></p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn More</a>
+    
+    </div>
+</div>
