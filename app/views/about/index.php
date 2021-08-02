0ec0d116e6d61458a40065856a40b85c043680e0
rlf990 <53922794+rlf990@users.noreply.github.com>
2021-08-02 14:19:10 +0700

    Update index.php

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,7 @@
+<div class="container">
 
-        <h1>About Me</h1>
-        <p>Yo! My  name <?= $data['nama']; ?>, student of <?= $data['sekolah']; ?></p>
+        <h1 class="mt-4">About Me</h1>
+        <img src="<?= BASEURL; ?>/img/MyProfile.jpg" alt="Rafi izzaturohman" width="400" class="rounded-circle">     
+        <p>Yo! My  name <?= $data['nama']; ?>, student of <?= $data['sekolah']; ?>.</p>
+        
+</div>
