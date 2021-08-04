502770e8a8c24e53a400ee18dfd566c7cefd1ee5
AiceKerime <rafiizzaturohman06@gmail.com>
2021-08-04 18:04:13 +0700

    Update index.php

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -2,7 +2,7 @@
 
     <div class="jumbotron mt-5">
         <h1 class="display-4">Selamat Datang di Website Saya</h1>
-        <p class="lead">Web ini dibuat dengan tujuan untuk praktik</p>
+        <p class="lead">Hai! Nama saya <?= $data['nama']; ?></p>
         <hr class="my-4">
         <p>Mengenai PHP MVC</p>
         <a class="btn btn-primary btn-lg" href="#" role="button">Learn More</a>
