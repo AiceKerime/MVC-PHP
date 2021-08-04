a965f3cb81117a0eb58014ce51c0939902ffc151
AiceKerime <rafiizzaturohman06@gmail.com>
2021-08-04 18:04:07 +0700

    Create Siswa_model.php

---
+++ app/models/Siswa_model.php
@@ -0,0 +1,24 @@
+<?php 
+
+class Siswa_model{
+    private $mhs = [
+        [
+           "nama" => "Rafi Izzaturohman",
+           "kelas" => "XII",
+           "jurusan" => "Rekayasa Perangkat Lunak",
+           "email" => "rfizza1405@gmail.com" 
+        ],
+        [
+           "nama" => "Mahesa Athur Thursena",
+           "kelas" => "XII",
+           "jurusan" => "Teknik Komputer dan Jaringan",
+           "email" => "mahesa22@gmail.com " 
+        ],
+        [
+           "nama" => "Fitri Nuraeni",
+           "kelas" => "XII",
+           "jurusan" => "Akomodasi Perhotelan",
+           "email" => "fnuraeni28@gmail.com" 
+        ]
+    ];
+}
