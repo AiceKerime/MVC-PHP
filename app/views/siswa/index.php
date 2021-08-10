a4d7c49ba547a437c1039c161596b68ed86a0985
AiceKerime <rafiizzaturohman06@gmail.com>
2021-08-10 12:39:51 +0700

    Update index.php

--- app/views/siswa/index.php
+++ app/views/siswa/index.php
@@ -4,6 +4,14 @@
         <div class="col-6">
             <h3>Daftar Siswa</h3>
 
+            <?php foreach( $data['mhs'] as $mhs ) : ?>
+            <ul>
+                <li><?= $mhs['nama']; ?></li>
+                <li><?= $mhs['kelas']; ?></li>
+                <li><?= $mhs['jurusan']; ?></li>
+                <li><?= $mhs['email']; ?></li>
+            </ul>
+            <?php endforeach; ?>
         </div>
     </div>
 
