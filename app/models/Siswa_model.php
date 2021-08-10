fe3aa499c44a912a2ec19a8e2ee35e8700632dc2
AiceKerime <rafiizzaturohman06@gmail.com>
2021-08-10 12:39:38 +0700

    Update Siswa_model.php

--- app/models/Siswa_model.php
+++ app/models/Siswa_model.php
@@ -1,24 +1,32 @@
 <?php 
 
 class Siswa_model{
-    private $mhs = [
-        [
-           "nama" => "Rafi Izzaturohman",
-           "kelas" => "XII",
-           "jurusan" => "Rekayasa Perangkat Lunak",
-           "email" => "rfizza1405@gmail.com" 
-        ],
-        [
-           "nama" => "Mahesa Athur Thursena",
-           "kelas" => "XII",
-           "jurusan" => "Teknik Komputer dan Jaringan",
-           "email" => "mahesa22@gmail.com " 
-        ],
-        [
-           "nama" => "Fitri Nuraeni",
-           "kelas" => "XII",
-           "jurusan" => "Akomodasi Perhotelan",
-           "email" => "fnuraeni28@gmail.com" 
-        ]
-    ];
+    // private $mhs = [
+    //     [
+    //        "nama" => "Rafi Izzaturohman",
+    //        "kelas" => "XII",
+    //        "jurusan" => "Rekayasa Perangkat Lunak",
+    //        "email" => "rfizza1405@gmail.com" 
+    //     ],
+    //     [
+    //        "nama" => "Mahesa Athur Thursena",
+    //        "kelas" => "XII",
+    //        "jurusan" => "Teknik Komputer dan Jaringan",
+    //        "email" => "mahesa22@gmail.com " 
+    //     ],
+    //     [
+    //        "nama" => "Fitri Nuraeni",
+    //        "kelas" => "XII",
+    //        "jurusan" => "Akomodasi Perhotelan",
+    //        "email" => "fnuraeni28@gmail.com" 
+    //     ]
+    // ];
+
+
+    public function getAllsiswa()
+        {
+            $this->stmt = $this->dbh->prepare('SELECT * FROM siswa');
+            $this->stmt->execute();
+            return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
 }
