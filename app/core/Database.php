c29847f34a8f335fbe5246917510622b327a62eb
AiceKerime <rafiizzaturohman06@gmail.com>
2021-08-14 06:46:16 +0700

    Update Database.php

--- app/core/Database.php
+++ app/core/Database.php
@@ -17,7 +17,7 @@ class Database {
 
         $option = [
             PDO::ATTR_PERSISTENT => true,
-            PDO::ATTR_ERRORMODE => PDO::ERRMODE_EXCEPTION
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ];
 
 
@@ -56,13 +56,15 @@ class Database {
 
     public function execute()
     {
-        $this->stmt->execute;
+        $this->stmt->execute();
     }
+
     public function resultSet()
     {
         $this->execute();
         return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
     }
+
     public function single()
     {
         $this->execute();
