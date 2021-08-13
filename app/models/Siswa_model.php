5cabd7f06868e1f246819f605c895a819a9bad6b
AiceKerime <rafiizzaturohman06@gmail.com>
2021-08-14 06:46:24 +0700

    Update Siswa_model.php

--- app/models/Siswa_model.php
+++ app/models/Siswa_model.php
@@ -1,32 +1,24 @@
 <?php 
 
 class Siswa_model{
-    // private $mhs = [
-    //     [
-    //        "nama" => "Rafi Izzaturohman",
-    //        "kelas" => "XII",
-    //        "jurusan" => "Rekayasa Perangkat Lunak",
-    //        "email" => "rfizza1405@gmail.com" 
-    //     ],
-    //     [
-    //        "nama" => "Mahesa Athur Thursena",
-    //        "kelas" => "XII",
-    //        "jurusan" => "Teknik Komputer dan Jaringan",
-    //        "email" => "mahesa22@gmail.com " 
-    //     ],
-    //     [
-    //        "nama" => "Fitri Nuraeni",
-    //        "kelas" => "XII",
-    //        "jurusan" => "Akomodasi Perhotelan",
-    //        "email" => "fnuraeni28@gmail.com" 
-    //     ]
-    // ];
+    private $table = 'siswa';
+    private $db;
 
+    public function __construct()
+    {
+     $this->db = new Database;   
+    }
 
     public function getAllsiswa()
         {
-            $this->stmt = $this->dbh->prepare('SELECT * FROM siswa');
-            $this->stmt->execute();
-            return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+            $this->db->query('SELECT * FROM ' . $this->table);
+            return $this->db->resultSet();
         }
+
+    public function getSiswaById($id)
+    {
+        $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id=:id');
+        $this->db->bind('id', $id);
+        return $this->db->single();
+    }
 }
