75312c4efb1d8812a4d79a437a67e713133d412a
AiceKerime <rafiizzaturohman06@gmail.com>
2021-08-25 22:50:45 +0700

    Update index.php

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -4,7 +4,7 @@
         <h1 class="display-4">Selamat Datang di Website Saya</h1>
         <p class="lead">Hai! Nama saya <?= $data['nama']; ?></p>
         <hr class="my-4">
-        <p>Mengenai PHP MVC</p>
+        <p>Web ini dibuat dengan tujuan untuk mempelajari materi tentang PHP MVC</p>
         <a class="btn btn-primary btn-lg" href="#" role="button">Learn More</a>
     
     </div>
