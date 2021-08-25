ba917754eec5fc786b9438df7f8b769128f616b6
AiceKerime <rafiizzaturohman06@gmail.com>
2021-08-25 22:49:26 +0700

    Update detail.php

--- app/views/siswa/detail.php
+++ app/views/siswa/detail.php
@@ -3,7 +3,7 @@
 <div class="card" style="width: 18rem;">
   <div class="card-body">
     <h5 class="card-title"><?= $data['mhs']['nama']; ?></h5>
-    <h6 class="card-subtitle mb-2 text-muted"><?= $data['mhs']['id']; ?></h6>
+    <h6 class="card-subtitle mb-2 text-muted"><?= $data['mhs']['nis']; ?></h6>
     <p class="card-text">Kelas : <?= $data['mhs']['kelas']; ?></p>
     <p class="card-text"><?= $data['mhs']['jurusan']; ?></p>
     <p class="card-text"><?= $data['mhs']['email']; ?></p>
