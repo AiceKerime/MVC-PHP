478b8138a349c00bb91ef4c1f39bd2c64589db80
AiceKerime <rafiizzaturohman06@gmail.com>
2021-09-02 02:17:24 +0700

    Create script.js

---
+++ public/js/script.js
@@ -0,0 +1,12 @@
+$(function() {
+
+    $('.tombolTambahData').on('click', function() {
+        $('#formModalLabel').html('Tambah Data Siswa')
+    });
+
+
+    $('.tampilModalUbah').on('click', function() {
+        $('#formModalLabel').html('Ubah Data Siswa')
+    });
+
+});
