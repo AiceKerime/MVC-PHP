b12056fb8c17978b49fd17200c67f35d7eda2d7c
AiceKerime <rafiizzaturohman06@gmail.com>
2021-09-02 08:35:14 +0700

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,13 @@
 $(function() {
 
     $('.tombolTambahData').on('click', function() {
-        $('#formModalLabel').html('Tambah Data Siswa')
+        $('#formModalLabel').html('Tambah Data Siswa');
     });
 
 
     $('.tampilModalUbah').on('click', function() {
-        $('#formModalLabel').html('Ubah Data Siswa')
+        $('#formModalLabel').html('Ubah Data Siswa');
+        $('.modal-footer button[type=submit]').html('Ubah Data')
     });
 
 });
